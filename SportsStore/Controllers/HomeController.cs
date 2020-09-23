3602b8938759e4ee89a1e8ccaf2204f4ac10be6d
Jeffrey Barton <jeffrey.barton99@outlook.com>
2020-09-23 12:12:39 -0500

    Initial commit

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController: Controller
+    {
+        private IStoreRepository repository;
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index(int productPage = 1)
+            => View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
+            //public IActionResult Index() => View(repository.Products);
+        
+    }
+}
